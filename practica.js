1a0c72c19d82b1befd8eeae0ededb1acd7d9f35d
Gerardobad <94128429+Gerardobad@users.noreply.github.com>
2022-03-27 22:29:39 -0600

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,4 @@
+console.log("\n************* Variables **************\n"); // Imprimir titulos 
+var numero1 = 4;
+var numero2 = 6;
+console.log("Número 1: " + numero1 + " Numero 2: " + numero2); // Imprimir  
